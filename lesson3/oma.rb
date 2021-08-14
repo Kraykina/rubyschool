fc3de99de5462280fc442891e5dd5e0e69566f13
Olga Lomakina <olga.kraykina904@gmail.com>
2021-08-14 16:22:59 +0100

    Lesson 3

---
+++ lesson3/oma.rb
@@ -0,0 +1,10 @@
+print "Enter voltage (220): "
+U = gets.to_i
+
+print "Enter resistance (10): "
+R = gets.to_i
+
+I = U/R
+
+puts "Result: " + I.to_s
+gets
---
+++ lesson3/soma.rb
@@ -0,0 +1,11 @@
+print "Enter a (1000): "
+a = gets
+
+print "Enter b (5): "
+b = gets.to_i
+
+print "Enter c (3.14): "
+c = gets.to_f
+
+x = a.to_i + b.to_i + c.to_f
+puts "Soma " + x.to_s
